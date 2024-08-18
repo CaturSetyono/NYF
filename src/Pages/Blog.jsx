3b37ceda603f1a6985bd453da384594409d36c5a
Catur setyono <catursetyono542@gmail.com>
2024-08-18 16:32:13 +0700

    fix bug

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -8,11 +8,11 @@ const Blog = () => {
         <div className="space-y-8">
           <div className="bg-gray-800 text-white p-6 rounded-lg shadow-lg flex flex-col space-y-4">
             <h2 className="text-2xl font-bold">Blog Post Title</h2>
-            <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet sapien nunc.</p>
+            <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet sapien nunc sit.</p>
           </div>
           <div className="bg-gray-800 text-white p-6 rounded-lg shadow-lg">
             <h2 className="text-2xl font-bold">Another Blog Post</h2>
-            <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet sapien nunc.</p>
+            <p className="mt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla sit amet sapien nunc sit.</p>
           </div>
         </div>
       </div>
