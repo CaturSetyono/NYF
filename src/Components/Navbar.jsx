ae82bd78c13c47508841a2c9fb4c04e8c83422d9
Catur setyono <catursetyono542@gmail.com>
2024-08-23 00:01:47 +0700

    navbar update

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,22 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { HashLink } from "react-router-hash-link";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const navRef = useRef();
+
+  const handleClickOutside = (event) => {
+    if (navRef.current && !navRef.current.contains(event.target)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
 
   return (
-    <nav className="bg-gray-950 bg-opacity-90 backdrop-blur-md shadow-lg fixed w-full top-0 left-0 z-50">
+    <nav
+      ref={navRef}
+      className="bg-gray-950 bg-opacity-90 backdrop-blur-md shadow-lg fixed w-full top-0 left-0 z-50"
+    >
       <div className="container mx-auto p-4 flex justify-between items-center">
         <div className="text-white font-bold text-lg">NYF</div>
         <div
           className="md:hidden text-white cursor-pointer"
           onClick={() => setIsOpen(!isOpen)}
         >
-          {isOpen ? "Close" : "Menu"}
+          {isOpen ? <AiOutlineClose size={24} /> : <AiOutlineMenu size={24} />}
         </div>
         <ul
-          className={`md:flex md:space-x-8 text-white font-semibold absolute md:static bg-gray-950 w-full left-0 md:w-auto md:bg-transparent transition-all duration-300 ease-in ${
+          className={`md:flex md:space-x-8 text-white font-semibold absolute md:static bg-gray-950 w-full left-0 md:w-auto md:bg-transparent transition-all duration-300 ease-in-out ${
             isOpen ? "top-16 opacity-100" : "top-[-300px] opacity-0"
           } md:opacity-100`}
         >
--- src/Components/Sosmed.jsx
+++ src/Components/Sosmed.jsx
@@ -1,12 +1,13 @@
 import React from "react";
+import { FaFacebook, FaDiscord, FaInstagram, FaLinkedin } from "react-icons/fa";
 
-const About = () => {
+const Sosmed= () => {
   return (
     <div
       id="sosmed"
       className="border-t-2 border-gray-800 min-h-screen flex flex-col justify-center items-center bg-gray-950 text-white pt-16 md:pt-8 px-4"
     >
-      <div className="min-h-screen bg-gray-950 text-white flex justify-center items-center pt-4 pb-4 ">
+      <div className="min-h-screen bg-gray-950 text-white flex justify-center items-center pt-4 pb-4">
         <div className="text-center">
           <h1 className="text-4xl font-bold mb-4">
             Connect with Us on Social Media
@@ -14,27 +15,27 @@ const About = () => {
           <div className="flex space-x-4 justify-center mt-16">
             <a
               href="https://web.facebook.com/profile.php?id=61562408777448"
-              className="bg-blue-500 p-4 rounded-lg hover:bg-blue-600"
+              className="p-4 rounded-lg hover:bg-blue-600"
             >
-              Facebook
+              <FaFacebook className="text-4xl" />
             </a>
             <a
               href="https://discord.gg/z4A33TWN"
-              className="bg-indigo-600 p-4 rounded-lg hover:bg-blue-500"
+              className="p-4 rounded-lg hover:bg-indigo-500"
             >
-              Discord
+              <FaDiscord className="text-4xl" />
             </a>
             <a
               href="https://www.instagram.com/notyourfriend_id/"
-              className="bg-gradient-to-r from-yellow-500 to-rose-600  p-4 rounded-lg hover:bg-gradient-to-r hover:from-rose-600 hover:to-yellow-500 "
+              className="p-4 rounded-lg hover:bg-gradient-to-r hover:from-rose-600 hover:to-yellow-500"
             >
-              Instagram
+              <FaInstagram className="text-4xl" />
             </a>
             <a
               href="https://www.linkedin.com/in/catursetyono/"
-              className="bg-blue-800 p-4 rounded-lg hover:bg-blue-500"
+              className="p-4 rounded-lg hover:bg-blue-500"
             >
-              Linkedin
+              <FaLinkedin className="text-4xl" />
             </a>
           </div>
         </div>
@@ -43,4 +44,4 @@ const About = () => {
   );
 };
 
-export default About;
+export default Sosmed;
