4cb0b7043f753df4374a507c7383afddc0c79a47
Catur setyono <catursetyono542@gmail.com>
2024-08-26 00:02:19 +0700

    Modifiy footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,11 +2,27 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-950 text-white py-6">
-      <div className="container mx-auto px-4">
-        <div className="text-center">
+    <footer className="bg-gray-950 text-white py-6 p-4 ">
+      <div className="flex mx-auto px-4 gap-4">
+        <div className="text-start">
           <p className="text-xs sm:text-sm md:text-base">
-            &copy; {new Date().getFullYear()} NYF. All rights reserved.
+             []-icon
+          </p>
+          <p className="mt-2 text-xs sm:text-sm md:text-base">
+            This website is designed to provide the best experience for our users. Feel free to contact us with any inquiries or feedback guys.
+          </p>
+        </div>
+        <div className="text-start">
+          <p className="text-xs sm:text-sm md:text-base">
+             []-icon
+          </p>
+          <p className="mt-2 text-xs sm:text-sm md:text-base">
+            This website is designed to provide the best experience for our users. Feel free to contact us with any inquiries or feedback guys.
+          </p>
+        </div>
+        <div className="text-start">
+          <p className="text-xs sm:text-sm md:text-base">
+             []-icon
           </p>
           <p className="mt-2 text-xs sm:text-sm md:text-base">
             This website is designed to provide the best experience for our users. Feel free to contact us with any inquiries or feedback guys.
