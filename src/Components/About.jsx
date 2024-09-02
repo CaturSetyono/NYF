a1756eb2eaae0f1c4158f2c43b87571d4e2cab2a
Catur setyono <catursetyono542@gmail.com>
2024-09-02 23:47:01 +0700

    add Service Components

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -6,9 +6,16 @@ function About() {
       id="about"
       className="border-t-2 border-gray-800 min-h-screen flex flex-col justify-center items-center bg-gray-950 text-white pt-8 pb-8 md:pt-16 px-4"
     >
-      <div className="flex flex-col md:flex-row max-w-5xl w-full p-8 gap-8">
+      <h2 className="text-3xl md:text-4xl font-bold mb-4 ">About Us</h2>
+      <div className="flex flex-col md:flex-row max-w-5xl w-full p-8 gap-8 text-center">
+        <div className="flex-1 flex justify-center items-center">
+          <img
+            className="rounded-lg shadow-lg max-w-[300px] h-auto"
+            src={nyfImage}
+            alt="About Us"
+          />
+        </div>
         <div className="flex-1 text-justify">
-          <h2 className="text-3xl md:text-4xl font-bold mb-4">About Us</h2>
           <p className="text-gray-300 mb-8 leading-relaxed">
             NYF (notyourfriend_id) is a content creator who focuses on on
             information technology and current issues around tech and IT
@@ -16,23 +23,14 @@ function About() {
             not only shares in-depth knowledge about web development, but also
             provides also provides innovative solutions to various technology
             challenges. With its modern style and up-to-date content, NYF is a
-            source of inspiration for tech enthusiasts and IT professionals.
+            source of inspiration for tech enthusiasts and IT professionals.<br /> We
+            also open cooperation in various forms, such as affiliate promotion,
+            paid promotion, and endorsement. If you are interested in working
+            with NYF to promote your product or service, don't hesitate to
+            contact us! or your services, don't hesitate to contact us! Follow
+            NYF to get the latest insights and practical solutions in the world
+            of information technology!
           </p>
-          <p className="text-gray-300 mb-8 leading-relaxed">
-            We also open cooperation in various forms, such as affiliate
-            promotion, paid promotion, and endorsement. If you are interested in
-            working with NYF to promote your product or service, don't hesitate
-            to contact us! or your services, don't hesitate to contact us!
-            Follow NYF to get the latest insights and practical solutions in the
-            world of information technology!
-          </p>
-        </div>
-        <div className="flex-1 flex justify-center items-center">
-          <img
-            className="rounded-lg shadow-lg max-w-[300px] h-auto"
-            src={nyfImage}
-            alt="About Us"
-          />
         </div>
       </div>
     </div>
--- src/Components/Service.jsx
+++ src/Components/Service.jsx
@@ -1,12 +1,19 @@
 import React from "react";
+import { RiAdvertisementLine } from "react-icons/ri";
+import { TbAffiliateFilled } from "react-icons/tb";
+import { TfiSupport } from "react-icons/tfi";
 
 const Service = () => {
-
-    return (
-        <div className="border-t-2 border-gray-800 min-h-screen justify-center items-center bg-gray-950 text-white pt-8 pb-8 md:pt-16 px-4">
-            
-        </div>
-    )
-}
+  return (
+    <div className="border-t-2 border-gray-800 min-h-screen justify-center items-center bg-gray-950 text-white  md:pt-16 px-4 text-center">
+          <h2 className="text-3xl md:text-4xl font-bold mb-4 ">Service</h2>
+          <div className="flex flex-col md:flex-row max w-full p-8 gap-8 justify-cente items-center">
+              <div className="flex-1 flex justify-center items-center w-full p-8"><RiAdvertisementLine className=" sm:w-24 sm:h-24 "/></div>
+              <div className="flex-1 flex justify-center items-center w-full p-8"><TbAffiliateFilled className=" sm:w-24 sm:h-24 "/></div>
+              <div className="flex-1 flex justify-center items-center w-full p-8"><TfiSupport className=" sm:w-24 sm:h-24 "/></div>
+          </div>
+    </div>
+  );
+};
 
 export default Service;
