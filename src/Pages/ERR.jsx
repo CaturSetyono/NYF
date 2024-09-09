bc6b8d5e902673febb2b8fa9aac0d894cfa7b70e
Catur setyono <catursetyono542@gmail.com>
2024-09-09 22:51:34 +0700

    modify error pages

--- src/Pages/ERR.jsx
+++ src/Pages/ERR.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const ErrorPage = () => {
-  return <div id="error" className="bg-gray-950 text-white w-full h-screen flex justify-center items-center">404 PAGE NOT FOUND</div>;
+  return (
+    <div
+      id="error"
+      className="bg-gray-950 text-white w-full h-screen flex justify-center items-center"
+    >
+      <h1 className="text-5xl font-bold animate-bounce">404 PAGE NOT FOUND</h1>
+    </div>
+  );
 };
 
 export default ErrorPage;
