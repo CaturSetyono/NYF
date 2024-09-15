fd05b8582588c7565b306dd2a678daf07701b315
Catur setyono <catursetyono542@gmail.com>
2024-09-15 16:03:05 +0700

    add responsive mobile to latest components

--- src/Components/Latest.jsx
+++ src/Components/Latest.jsx
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 import projectImg from "../assets/illustration-article.svg";
+
 const Latest = () => {
-  
   const projects = [
     {
       id: 1,
@@ -24,32 +24,32 @@ const Latest = () => {
   ];
 
   return (
-    <div className="border-t-2 border-gray-800 min-h-screen flex flex-col justify-center items-center bg-gray-950 text-white  text-center p-24">
-      <h2 className="text-3xl md:text-4xl font-bold mb-16 ">Latest Project</h2>
-      <div className="space-y-6 w-full max-w-4xl">
+    <div className="border-t-2 border-gray-800 min-h-screen flex flex-col justify-center items-center bg-gray-950 text-white text-center p-6 md:p-24">
+      <h2 className="text-2xl md:text-4xl font-bold mb-8 md:mb-16">Latest Project</h2>
+      <div className="space-y-6 w-full max-w-xl md:max-w-4xl">
         {projects.map((project) => (
           <div
             key={project.id}
-            className="flex items-center bg-black/50 p-6 rounded-xl shadow-lg"
+            className="flex flex-col md:flex-row items-center bg-black/50 p-4 md:p-6 rounded-xl shadow-lg"
           >
-            <div className="w-24 h-24 flex items-center justify-center bg-gray-800 rounded-lg mr-6">
+            <div className="w-20 h-20 md:w-24 md:h-24 flex items-center justify-center bg-gray-800 rounded-lg mb-4 md:mb-0 md:mr-6">
               {project.image ? (
                 <img
                   src={project.image}
                   alt={project.title}
-                  className="w-fit h-full object-cover rounded-lg hover:shadow-md  hover:shadow-sky-500 hover:transition-all"
+                  className="w-full h-full object-cover rounded-lg hover:shadow-md hover:shadow-sky-500 hover:transition-all"
                 />
               ) : (
                 <div className="text-gray-500">No Image</div>
               )}
             </div>
-            <div className="flex-1">
-              <h2 className="text-xl font-semibold">{project.title}</h2>
+            <div className="flex-1 text-center md:text-left">
+              <h2 className="text-lg md:text-xl font-semibold">{project.title}</h2>
             </div>
-            <div>
+            <div className="mt-4 md:mt-0">
               <a
                 href={project.details}
-                className="px-4 py-2 bg-purple-500 text-white rounded-lg hover:bg-purple-600 transition-all"
+                className="px-4 py-2 bg-sky-600 text-white rounded-md hover:bg-purple-600 transition-all"
               >
                 View Details
               </a>
